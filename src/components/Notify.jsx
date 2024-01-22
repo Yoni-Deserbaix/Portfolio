8835741de9ec00fefddf26a0650c18265e9023e6
unknown <yonideserbaix@gmail.com>
2024-01-22 09:53:47 +0100

    update

--- src/components/Notify.jsx
+++ src/components/Notify.jsx
@@ -1,40 +1,40 @@
-// import React, { useEffect } from "react";
-// import { ToastContainer, toast } from "react-toastify";
-// import "react-toastify/dist/ReactToastify.css";
-// import { Bounce } from "react-toastify";
+import React, { useEffect } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { Bounce } from "react-toastify";
 
-// const Notify = () => {
-//   useEffect(() => {
-//     toast.success(" Disponible pour une alternance en septembre !", {
-//       position: "bottom-left",
-//       autoClose: 5000,
-//       hideProgressBar: false,
-//       closeOnClick: true,
-//       pauseOnHover: true,
-//       draggable: true,
-//       progress: undefined,
-//       theme: "light",
-//       transition: Bounce,
-//     });
-//   }, []);
+const Notify = () => {
+  useEffect(() => {
+    toast.success(" Disponible pour une alternance en septembre !", {
+      position: "bottom-left",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "light",
+      transition: Bounce,
+    });
+  }, []);
 
-//   return (
-//     <div>
-//       <ToastContainer
-//         position="bottom-right"
-//         autoClose={5000}
-//         hideProgressBar={false}
-//         newestOnTop={false}
-//         closeOnClick
-//         rtl={false}
-//         pauseOnFocusLoss
-//         draggable
-//         pauseOnHover
-//         theme="dark"
-//         transition={Bounce}
-//       />
-//     </div>
-//   );
-// };
+  return (
+    <div>
+      <ToastContainer
+        position="bottom-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+        transition={Bounce}
+      />
+    </div>
+  );
+};
 
-// export default Notify;
+export default Notify;
