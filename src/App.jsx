2db1bd544643c72ad1b930cfc5e35bdfaad4e210
unknown <yonideserbaix@gmail.com>
2024-02-01 15:47:05 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Header from './components/Header/Header';
 import Home from './components/Home/Home';
 import About from './components/About/About';
 import Projects from './components/Projects/Projects';
-import Notify from './components/notify';
+import Notify from './components/Notify';
 import Skills from './components/Skills/Skills';
 import Footer from './components/Footer/Footer';
 import Contact from './components/Contact/Contact';
