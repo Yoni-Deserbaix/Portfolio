9c5c14fbd9f74c7e7146a14f8ceb42d3e55a80dc
unknown <yonideserbaix@gmail.com>
2024-03-26 21:52:46 +0100

    update bg style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,10 +7,15 @@ import Skills from "./components/Skills/Skills";
 
 export default function Home() {
   return (
-    <div className="max-w-7xl mx-auto">
-      <div className=" mx-auto overflow-hidden dark:bg-black bg-white  dark:bg-grid-white/[0.2] bg-grid-black/[0.2]">
-        <Header />
-        <Hero />
+    <div
+      className="min-h-screen bg-black mx-auto overflow-hidden 
+        "
+    >
+      <div className="dark:bg-black bg-white  dark:bg-grid-white/[0.05] bg-grid-black/[0.2]">
+        <div className="max-w-7xl mx-auto">
+          <Header />
+          <Hero />
+        </div>
       </div>
       <Projects />
       <Skills />
