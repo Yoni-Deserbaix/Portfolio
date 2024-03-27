aa313297889432e2ae288c2231c3a8a78ebd1d3f
unknown <yonideserbaix@gmail.com>
2024-03-27 22:04:39 +0100

    update projects component

--- src/app/components/Projects/Projects.tsx
+++ src/app/components/Projects/Projects.tsx
@@ -1,31 +1,57 @@
 import React from "react";
 import Image from "next/image";
+import { title } from "process";
+import {
+  SiCss3,
+  SiFigma,
+  SiHtml5,
+  SiJavascript,
+  SiNextdotjs,
+  SiReact,
+  SiReactrouter,
+  SiSass,
+  SiTailwindcss,
+  SiTypescript,
+} from "react-icons/si";
+import Title from "../ui/Title";
 
 export default function Projects() {
+  const projects = [
+    {
+      title: "Portfolio",
+      stack: [SiNextdotjs, SiTypescript, SiTailwindcss, SiFigma],
+      link: "http://localhost:3000",
+      cover: "./project01.png",
+      background: "bg-indigo-500",
+    },
+    {
+      title: "MyBlog",
+      stack: [SiNextdotjs, SiTypescript, SiTailwindcss],
+      link: "http://localhost:3000",
+      cover: "./project02.png",
+      background: "bg-green-500",
+    },
+    {
+      title: "CineQwik",
+      stack: [SiReact, SiReactrouter, SiCss3],
+      link: "http://localhost:3000",
+      cover: "./project03.png",
+      background: "bg-blue-500",
+    },
+    {
+      title: "GTA VI Landing Page",
+      stack: [SiHtml5, SiSass, SiJavascript],
+      link: "http://localhost:3000",
+      cover: "./project04.png",
+      background: "bg-pink-500",
+    },
+  ];
   return (
-    <section
-      className="work-section  w-3/5 m-auto px-8 py-[100px] max-md:w-full max-md:m-auto max-md:py-24"
-      id="projects"
-    >
-      <h3
-        className="section-title text-[1.3rem] font-extrabold uppercase pb-8"
-        data-aos="fade-up"
-        data-aos-duration="300"
-      >
-        Projets
-      </h3>
-      <div
-        className="subtitle"
-        data-aos="fade-up"
-        data-aos-delay="500"
-        data-aos-duration="300"
-      >
-        <div className="section-subtitle  text-[#767676]">
-          Parce-que <b>quelques images</b> valent mieux que{" "}
-          <b>10 000 lignes de code</b>, je vous laisse découvrir ci-dessous
-          quelques <b>projets</b> que j'ai pu réaliser !
-        </div>
-      </div>
+    <section className="py-10 p-5 sm:p-0" id="projects">
+      <Title
+        text="Projects 🎨"
+        className="flex flex-col mt-28 items-center justify-center text-3xl rotate-6"
+      />
     </section>
   );
 }
