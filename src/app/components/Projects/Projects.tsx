416b491bf076b589238e2214458588dab01447f2
unknown <yonideserbaix@gmail.com>
2024-03-28 14:02:40 +0100

    upadate projects section

--- src/app/components/Projects/Projects.tsx
+++ src/app/components/Projects/Projects.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import Image from "next/image";
-import { title } from "process";
 import {
   SiCss3,
   SiFigma,
@@ -16,6 +15,7 @@ import {
 import Title from "../../../components/ui/Title";
 import { Link } from "lucide-react";
 import { cn } from "@/lib/utils";
+import { DirectionAwareHover } from "@/components/ui/direction-aware-hover";
 
 export default function Projects() {
   const projects = [
@@ -23,33 +23,33 @@ export default function Projects() {
       title: "Portfolio",
       stack: [SiNextdotjs, SiTypescript, SiTailwindcss, SiFigma],
       link: "http://localhost:3000",
-      cover: "./project01.png",
+      cover: "/assets/projects/project01.png",
       background: "bg-indigo-500",
     },
     {
       title: "MyBlog",
       stack: [SiNextdotjs, SiTypescript, SiTailwindcss],
       link: "http://localhost:3000",
-      cover: "./project02.png",
+      cover: "/assets/projects/project02.png",
       background: "bg-green-500",
     },
     {
       title: "CineQwik",
       stack: [SiReact, SiReactrouter, SiCss3],
       link: "http://localhost:3000",
-      cover: "./project03.png",
+      cover: "/assets/projects/project03.png",
       background: "bg-blue-500",
     },
     {
       title: "GTA VI Landing Page",
       stack: [SiHtml5, SiSass, SiJavascript],
       link: "http://localhost:3000",
-      cover: "./project04.png",
+      cover: "/assets/projects/project04.png",
       background: "bg-pink-500",
     },
   ];
   return (
-    <section className="py-10 p-5 sm:p-0" id="projects">
+    <div className="py-10 p-5 sm:p-0" id="projects">
       <Title
         text="Projects 🎨"
         className="flex flex-col mt-28 items-center justify-center text-3xl rotate-6"
@@ -59,12 +59,15 @@ export default function Projects() {
         {projects.map((project, index) => {
           return (
             <Link href={project.link} key={index}>
-              <div className={cn("p-5 rounded-md", project.background)}></div>
+              <div className={cn("p-5 rounded-md", project.background)}>
+                <DirectionAwareHover imageUrl={project.cover}>
+                  <h2>{project.title}</h2>
+                </DirectionAwareHover>
+              </div>
             </Link>
           );
         })}
-        ;
       </div>
-    </section>
+    </div>
   );
 }
