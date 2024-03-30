ae1e7f8328cc60961f5ec45fdf3a6a24ec1a4951
unknown <yonideserbaix@gmail.com>
2024-03-30 16:09:43 +0100

    update responsive page

--- src/app/project/[id]/page.tsx
+++ src/app/project/[id]/page.tsx
@@ -19,52 +19,55 @@ export default function Home({ params }: ProjectType) {
   const project: any = ProjectsData.find(
     (project) => project.id.toString() === projectId
   );
-
   return (
-    <div className="max-w-7xl mx-auto max-xl:px-8">
-      <div className="text-2xl font-bold underline underline-offset-8 decoration-green-500 -rotate-2 pt-5">
-        <Link href="/">Yoni Deserbaix 🦇</Link>
+    <div className="max-w-7xl mx-auto max-xl:px-8 flex flex-wrap">
+      <div className="w-2/3 max-md:w-full ">
+        <div className="text-2xl font-bold underline underline-offset-8 decoration-green-500 -rotate-2 pt-5">
+          <Link href="/">Yoni Deserbaix 🦇</Link>
+        </div>
+        <Title
+          text={project.title}
+          className="flex flex-col mt-28 items-start justify-center text-3xl"
+        />
+        <div className="space-y-6">
+          <div className="text-xl font-bold pt-12">{project.subtitle}</div>
+          <p className="md:w-96 text-md text-gray-300 max-sm:text-base">
+            {project.description}
+          </p>
+          <div className="text-xl font-bold">{project.secondSubtitle}</div>
+          <ul className="md:w-96 text-md text-gray-300 max-sm:text-base">
+            {project.skillsGained.map((skill: string, index: number) => (
+              <li key={index}>{skill}</li>
+            ))}
+          </ul>
+          <div className="text-xl font-bold">{project.thirdSubtitle}</div>
+          <div className="flex gap-5 ml-2">
+            {project.stack.map((Icon: IconType, index: number) => {
+              return <Icon className="w-8 h-8" key={index} />;
+            })}
+          </div>
+          <div className="flex gap-20 max-sm:flex-col max-sm:gap-0">
+            <a
+              href={project.githubLink}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <Title
+                text="Code 🚀"
+                className="flex flex-col mt-12 items-start justify-center text-3xl "
+              />
+            </a>
+            <a href={project.demo} target="_blank" rel="noopener noreferrer">
+              <Title
+                text="Demo ✨"
+                className="flex flex-col mt-12 items-start justify-center text-3xl "
+              />
+            </a>
+          </div>
+        </div>
       </div>
-      <Title
-        text={project.title}
-        className="flex flex-col mt-28 items-start justify-center text-3xl"
-      />
-      <div className="space-y-6">
-        <div className="text-xl font-bold pt-12">{project.subtitle}</div>
-        <p className="md:w-96 text-md text-gray-300 max-sm:text-base">
-          {project.description}
-        </p>
-        <div className="text-xl font-bold">{project.secondSubtitle}</div>
-        <ul className="md:w-96 text-md text-gray-300 max-sm:text-base">
-          {project.skillsGained.map((skill: string, index: number) => (
-            <li key={index}>{skill}</li>
-          ))}
-        </ul>
-        <div className="text-xl font-bold">{project.thirdSubtitle}</div>
-        <div className="flex gap-5 ml-2">
-          {project.stack.map((Icon: IconType, index: number) => {
-            return <Icon className="w-8 h-8" key={index} />;
-          })}
-        </div>
-        <div className="flex gap-20 max-sm:flex-col max-sm:gap-0">
-          <a
-            href={project.githubLink}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Title
-              text="Code 🚀"
-              className="flex flex-col mt-12 items-start justify-center text-3xl "
-            />
-          </a>
-          <a href={project.demo} target="_blank" rel="noopener noreferrer">
-            <Title
-              text="Demo ✨"
-              className="flex flex-col mt-12 items-start justify-center text-3xl "
-            />
-          </a>
-        </div>
-        <div className="grid grid-cols-1 pt-20 gap-5 w-1/2 max-sm:w-full">
+      <div className="w-1/3 max-md:w-full">
+        <div className="grid grid-cols-1 pt-12 md:pt-40 gap-5 w-full">
           {project.cover.map((imageCover: string, index: number) => (
             <div
               key={index}
@@ -72,7 +75,7 @@ export default function Home({ params }: ProjectType) {
             >
               <DirectionAwareHover
                 imageUrl={imageCover}
-                className="w-full space-y-5 cursor-pointer"
+                className="w-full space-y-5"
               >
                 {""}
               </DirectionAwareHover>
