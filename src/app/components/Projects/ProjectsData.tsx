00d3040681e72918e2625c5c1eec96ea7674fc83
unknown <yonideserbaix@gmail.com>
2024-03-30 11:23:06 +0100

    create projects page

--- src/app/components/Projects/ProjectsData.tsx
+++ src/app/components/Projects/ProjectsData.tsx
@@ -14,30 +14,37 @@ import {
 
 export const ProjectsData = [
   {
+    id: 1,
     title: "Portfolio",
     stack: [SiNextdotjs, SiTypescript, SiTailwindcss, SiFigma],
-    link: "http://localhost:3000",
+    link: "/project/1",
     cover: "/assets/projects/project01.png",
     background: "bg-indigo-500",
   },
   {
+    id: 2,
+
     title: "MyBlog",
     stack: [SiNextdotjs, SiTypescript, SiTailwindcss],
-    link: "http://localhost:3000",
+    link: "/project/2",
     cover: "/assets/projects/project02.png",
     background: "bg-green-500",
   },
   {
+    id: 3,
+
     title: "CineQwik",
     stack: [SiReact, SiReactrouter, SiAxios],
-    link: "http://localhost:3000",
+    link: "/project/3",
     cover: "/assets/projects/project03.png",
     background: "bg-green-500",
   },
   {
+    id: 4,
+
     title: "GTA VI Landing Page",
     stack: [SiHtml5, SiSass, SiJavascript],
-    link: "http://localhost:3000",
+    link: "/project/4",
     cover: "/assets/projects/project04.png",
     background: "bg-indigo-500",
   },
