2579d8300480c5bcb75c68066498012f18df450b
unknown <yonideserbaix@gmail.com>
2024-04-01 12:52:50 +0200

    add: metadata for SEO

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,11 +6,25 @@ import { ThemeProvider } from "../components/ui/theme-provider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Yoni - Développeur Frontend",
-  description: "Yoni - Développeur Frontend",
-  icons: {
-    icon: "/favicon.svg",
+  metadataBase: new URL("https://yoni-deserbaix-potfolio.vercel.app/"),
+
+  title: "Yoni Deserbaix - Développeur Frontend",
+  authors: {
+    name: "Yoni Deserbaix",
   },
+
+  description:
+    "Basé à Nantes, je suis un développeur Frontend passionné par la construction d'applications web modernes que les utilisateurs adorent.",
+  openGraph: {
+    title: "Yoni Deserbaix - Développeur Frontend",
+    description:
+      "Basé à Nantes, je suis un développeur Frontend passionné par la construction d'applications web modernes que les utilisateurs adorent.",
+    url: "https://yoni-deserbaix-potfolio.vercel.app/",
+    siteName: "Yoni Deserbaix",
+    images: "/assets/projects/project01.png",
+    type: "website",
+  },
+  keywords: ["Portfolio", "Yoni Deserbaix", "Développeur Frontend"],
 };
 
 export default function RootLayout({
