d19e622f3bc919451680ac0df40ba532d4148f17
unknown <yonideserbaix@gmail.com>
2024-04-03 10:14:56 +0200

    remove: cursor pointer

--- src/components/ui/card-hover-effect.tsx
+++ src/components/ui/card-hover-effect.tsx
@@ -48,7 +48,7 @@ export const HoverEffect = ({
                 />
               )}
             </AnimatePresence>
-            <div className="rounded-md w-full p-4 overflow-hidden bg-black hover:ring-2 ring-green-500 relative z-20 transition-all duration-500 cursor-pointer">
+            <div className="rounded-md w-full p-4 overflow-hidden bg-black hover:ring-2 ring-green-500 relative z-20 transition-all duration-500">
               <div className="py-10 z-50 relative space-y-50">
                 <Icon className="w-8 h-8 mx-auto" />
                 <p className="text-sm pt-2 font-bold text-center text-gray-300">
